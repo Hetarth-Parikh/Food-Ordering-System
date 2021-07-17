768b5523b04a4bb7e1f5c2054e9fcf537dcaec34
Hetarth <19bce155@nirmauni.ac.in>
2021-07-18 00:59:10 +0530

    Trying to resolve error page

--- backend/app.js
+++ backend/app.js
@@ -14,6 +14,10 @@ app.use(require('./router/auth'));
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('build'));
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+    });
 }
 
 app.listen(PORT, () => {
