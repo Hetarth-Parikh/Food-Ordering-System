ca45ffcf2bab411bcafd49881f282974f804b90f
Hetarth <19bce155@nirmauni.ac.in>
2021-07-18 00:48:40 +0530

    BrowseRouter updated to HashRouter

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 
 ReactDom.render(
   <>
-    <BrowserRouter>
-      <App/>
-    </BrowserRouter>
+    <HashRouter>
+      <App />
+    </HashRouter>
   </>,
   document.getElementById('root')
 )
